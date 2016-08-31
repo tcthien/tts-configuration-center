275e154884ec27a9cd57a16e9700b0de83c75a1b
Thien Tran <thientran1986@gmail.com>
2016-08-31 11:17:43 +0700

    Update testcase

--- sources/tts-configcenter-service/src/test/java/com/tts/app/configcenter/service/ssh/cmd/DockerFeatureTest.java
+++ sources/tts-configcenter-service/src/test/java/com/tts/app/configcenter/service/ssh/cmd/DockerFeatureTest.java
@@ -10,36 +10,35 @@ import com.tts.app.configcenter.service.ssh.feature.DockerFeature;
 
 public class DockerFeatureTest {
 
-//    private SSHCommandExecutor executor;
-//    private DockerFeature feature;
-//    
-//    public DockerFeatureTest() {
-//        executor = new SSHCommandExecutorImpl();
-//        feature = new DockerFeature();
-//        feature.setCommandExecutor(executor);
-//    }
-//    
-//    public void testCheck() throws Exception {
-//        Server server = createServer();
-//        boolean status = feature.check(server);
-//        Assert.assertTrue(status);
-//    }
-//
-//    protected Server createServer() {
-//        Server server = new Server();
-//        server.setIpAddress("192.168.100.100");
-//        server.setUserName("tcthien");
-//        server.setPassword("tcthien");
-//        return server;
-//    }
-//    
-//    public void testUninstallDocker() throws Exception {
-//        SSHResult rs = feature.uninstall(createServer());
-//        Assert.assertEquals(SSHResult.STATUS_OK, rs.getExistStatus());
-//    }
-//    
-//    public void testInstallDocker() throws Exception {
-//        SSHResult rs = feature.install(createServer());
-//        Assert.assertEquals(SSHResult.STATUS_OK, rs.getExistStatus());
-//    }
+    private SSHCommandExecutor executor;
+    private DockerFeature feature;
+    
+    public DockerFeatureTest() {
+        executor = new SSHCommandExecutorImpl();
+        feature = new DockerFeature(executor);
+    }
+    
+    public void testCheck() throws Exception {
+        Server server = createServer();
+        boolean status = feature.check(server);
+        Assert.assertTrue(status);
+    }
+
+    protected Server createServer() {
+        Server server = new Server();
+        server.setIpAddress("192.168.100.100");
+        server.setUserName("tcthien");
+        server.setPassword("tcthien");
+        return server;
+    }
+    
+    public void testUninstallDocker() throws Exception {
+        SSHResult rs = feature.uninstall(createServer());
+        Assert.assertEquals(SSHResult.STATUS_OK, rs.getExistStatus());
+    }
+    
+    public void testInstallDocker() throws Exception {
+        SSHResult rs = feature.install(createServer());
+        Assert.assertEquals(SSHResult.STATUS_OK, rs.getExistStatus());
+    }
 }
